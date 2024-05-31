ac2289b1d8ccaa003644e54c821f860d03135f5d
05tushar1997 <161101934+05tushar1997@users.noreply.github.com>
2024-05-31 23:40:57 +0530

    Add files via upload

---
+++ University_Education_Analytics.sql
@@ -0,0 +1,7 @@
+create database University_Education_Analytics
+use University_Education_Analytics
+select * from students
+select * from grades
+select * from faculty
+select * from courses
+select * from budgets
